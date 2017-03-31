040d05b4f8cfbbd5e0d5486fd94e4a3474c84ae0
MultiCampus1102 <dlrlftjs12@gmail.com>
2017-03-31 09:24:44 +0900

    session객체 스프링으로 관리

--- Day0330ex/src/dao/ProductDao.java
+++ Day0330ex/src/dao/ProductDao.java
@@ -11,67 +11,39 @@ import org.springframework.stereotype.Component;
 import model.Product;
 @Component
 public class ProductDao implements IProductDao{
+//	@Autowired
+//	private SqlSessionFactory sqlSessionFactory;
 	@Autowired
-	private SqlSessionFactory sqlSessionFactory;
+	private SqlSession session;
 	
 	@Override
 	public int insertProduct(HashMap<String, Object> params) {
 		// TODO Auto-generated method stub
-		SqlSession session = sqlSessionFactory.openSession();
-		try{
-//			return session.insert("product.insertProduct", product);
-			return session.getMapper(IProductDao.class).insertProduct(params);
-		} finally {
-			session.close();
-		}
+		return session.getMapper(IProductDao.class).insertProduct(params);
 	}
 
 	@Override
 	public int updateProduct(HashMap<String, Object> params) {
 		// TODO Auto-generated method stub
-		SqlSession session = sqlSessionFactory.openSession();
-		try{
-//			return session.update("product.updateProduct", product);
-			return session.getMapper(IProductDao.class).updateProduct(params);
-		}finally {
-			session.close();
-		}
+		return session.getMapper(IProductDao.class).updateProduct(params);
 	}
 
 	@Override
 	public int deleteProduct(int code) {
 		// TODO Auto-generated method stub
-		SqlSession session = sqlSessionFactory.openSession();
-		try{
-//			return session.delete("product.deleteProduct", code);
-			return session.getMapper(IProductDao.class).deleteProduct(code);
-		}finally {
-			session.close();
-		}
+		return session.getMapper(IProductDao.class).deleteProduct(code);
 	}
 
 	@Override
 	public Product selectOne(int code) {
 		// TODO Auto-generated method stub
-		SqlSession session = sqlSessionFactory.openSession();
-		try{
-//			return session.selectOne("product.selectOne", code);
-			return session.getMapper(IProductDao.class).selectOne(code);
-		}finally {
-			session.close();
-		}
+		return session.getMapper(IProductDao.class).selectOne(code);
 	}
 
 	@Override
 	public List<HashMap<String, Object>> selectAll() {
 		// TODO Auto-generated method stub
-		SqlSession session = sqlSessionFactory.openSession();
-		try{
-//			return session.selectList("product.selectAll");
-			return session.getMapper(IProductDao.class).selectAll();
-		}finally {
-			session.close();
-		}
+		return session.getMapper(IProductDao.class).selectAll();
 	}
 
 }
